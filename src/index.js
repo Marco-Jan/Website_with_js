ab7b8ca5dce7fcf8a41c3f48c0e72f85e8f56a73
Iron-Baloou <marco.jan@gmx.de>
2023-07-06 00:13:20 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -4,28 +4,6 @@ import _ from "lodash";
 import "./style.css";
 
 
-class News {
-  constructor(date, time, newscontent){
-  
-    this.date = date;
-    this.time = time;
-    this.newscontent = newscontent;
-
-  }
-  getDate() {
-    return this.date;
-  }
-
-  getTime() {
-    return this.time;
-  }
-
-  getNews() {
-    return this.newscontent;
-  }
-}
-
-
 function createTag(parent_node, tag_node, id_name, class_name, content) {
   const tag = document.createElement(tag_node);
 
@@ -91,39 +69,65 @@ function createHeader(){
   const menu = createTag(nav_header, "ul", null, "menu_header");
 
   let menu_list = ["Home", "Our Team", "Contact"];
-  createMultiTags(menu, "li", menu_list.length, menu_list, true);
+  createMultiTags(menu, "li", menu_list.length, menu_list, 'hes');
 
-
+  
+  
   logo_header.src = '../src/img/Logo.png';
 }
 // Header end
 
-// Mainpage start
+// Mainpage start + News
 function createMainpage(){
 
   const main = createTag(null,'div', 'content_box')
-  const mainBg = createTag(main,'img', 'mainBGId')
-  const main2 = createTag(main,'div', 'main2Id')
+  const mainBg = createTag(main,'img', 'mainBGId',null,)
+  const main2 = createTag(main,'div', 'main2Id')  
   
   mainBg.src = "../src/img/MJ_Merano_tapas-5.jpg";
 
-  const news1Content = createTag(main2,'div', 'news1Content',null, 'News1')
-  // const newsText1 = createTag(news1Content,'p', 'newsText1Id',null, )
+  const news1Content = createTag(main2,'div', 'news1Content',null,)
+
+  const newsText1 = createTag(news1Content,'p', 'newsText1Id',null, )
   const newsImage1 = createTag(news1Content,'img', 'newsImage1Id')
   newsImage1.src = "../src/img/MJ_Merano_tapas-1.jpg";
 
-  const news2Content = createTag(main2,'div', 'news2Content',null, 'News2')
+  const news2Content = createTag(main2,'div', 'news2Content',null,)
+  const newsText2 = createTag(news2Content,'p', 'newsText2Id',null, )
   const newsImage2 = createTag(news2Content,'img', 'newsImage2Id')
   newsImage2.src = "../src/img/MJ_MG_04842018.jpg";
 
-  NewsInput.forEach((News) => {
-    
-      createCard(menu_coffee, drink, order);
-    
-   
-  });
+ 
 }
 
+// Mainpage end
+
+// Seiten Wechsel
+function changePage(){
+
+  const homeBtn = document.getElementById("Home");
+  homeBtn.addEventListener("click", function () {
+    console.log('es wurde geklickt');
+
+    window.location.href = "index.html";
+  });
+
+  const teamBtn = document.getElementsByClassName("Our Team");
+  teamBtn.addEventListener("click", function () {
+    console.log('es wurde geklickt');
+    window.location.href = "team.html";
+  }); 
+
+  const contactpage =document.URL.includes("contact.html");
+  const contactBtn = document.getElementById("Contact");
+  contactBtn.addEventListener("click", function () {
+    console.log('es wurde geklickt');
+
+    window.location.href = contactpage;
+  });
+}
+  
+
 
 
   
@@ -135,18 +139,15 @@ function createMainpage(){
 
 
 
-function initial(){
+function initial() {
 
-  const News1 = new News ('28.7.2023', '18:30','Beachparty');
-  const News2 = new News ('15.8.2023', '20:15', 'Midsummer Party');
 
-  const NewsInput = [News1, News2]; 
-
-  
 
   createHeader();
+  
   createMainpage();
-  createNews(NewsInput);
+  changePage();
+
 }
 
 initial();
